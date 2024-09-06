ae79193b940de868397ce27068871fe43edd4577
samee noushad <your.email@example.com>
2024-09-06 00:44:27 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './component/nav'
+
+export const App = () => {
+  return (
+    <>
+     <Nav/>
+    </>
+  )
+}
+export default App
---
+++ src/component/nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const    Nav = () => {
+  return (
+    <>
+        <h1>c<span>ode</span>Dazzaal</h1>
+
+        <div className='nav-link'>
+            <ul>
+                <li>
+                    <a href='#'>hHOME</a>
+                </li>
+                <li>
+                    <a href='#'>ABOUT</a>
+                </li>
+                <li>
+                    <a href='#'>PORTFOLIO</a>
+                </li>
+                <li>
+                    <a href='#'>CONTACT</a>
+                </li>
+            </ul>
+        </div>
+    </>
+  )
+}
+export default Nav
